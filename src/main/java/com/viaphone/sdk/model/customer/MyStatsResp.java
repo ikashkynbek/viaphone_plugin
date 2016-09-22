c380a3e1ea1942c701948ea212d4708be4019bdd
ik <ikashkynbek@gmail.com>
2016-09-22 12:01:50 +0600

    customer info fields

--- src/main/java/com/viaphone/sdk/model/customer/MyStatsResp.java
+++ src/main/java/com/viaphone/sdk/model/customer/MyStatsResp.java
@@ -2,12 +2,35 @@ package com.viaphone.sdk.model.customer;
 
 
 import com.viaphone.sdk.model.Response;
+import com.viaphone.sdk.model.enums.Gender;
+import com.viaphone.sdk.model.enums.SocialStatus;
+
+import java.util.Date;
 
 public class MyStatsResp extends Response {
 
+    private Long customerId;
     private String phone;
     private String name;
     private String email;
+    private String country;
+    private String state;
+    private String city;
+    private Date birthday;
+    private Gender gender;
+    private SocialStatus socialStatus;
+    private String facebookId;
+    private String googleId;
+    private String instagramId;
+    private String twitterId;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
 
     public String getPhone() {
         return phone;
@@ -33,11 +56,102 @@ public class MyStatsResp extends Response {
         this.email = email;
     }
 
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public SocialStatus getSocialStatus() {
+        return socialStatus;
+    }
+
+    public void setSocialStatus(SocialStatus socialStatus) {
+        this.socialStatus = socialStatus;
+    }
+
+    public String getFacebookId() {
+        return facebookId;
+    }
+
+    public void setFacebookId(String facebookId) {
+        this.facebookId = facebookId;
+    }
+
+    public String getGoogleId() {
+        return googleId;
+    }
+
+    public void setGoogleId(String googleId) {
+        this.googleId = googleId;
+    }
+
+    public String getInstagramId() {
+        return instagramId;
+    }
+
+    public void setInstagramId(String instagramId) {
+        this.instagramId = instagramId;
+    }
+
+    public String getTwitterId() {
+        return twitterId;
+    }
+
+    public void setTwitterId(String twitterId) {
+        this.twitterId = twitterId;
+    }
+
     @Override
     public String toString() {
-        return "\n\tphone: " + phone +
+        return "\n\tcustomerId: " + customerId +
+                "\n\tphone: " + phone +
                 "\n\tname: " + name +
                 "\n\temail: " + email +
+                "\n\tcountry: " + country +
+                "\n\tstate: " + state +
+                "\n\tcity: " + city +
+                "\n\tbirthday: " + birthday +
+                "\n\tgender: " + gender +
+                "\n\tsocialStatus: " + socialStatus +
+                "\n\tfacebookId: " + facebookId +
+                "\n\tgoogleId: " + googleId +
+                "\n\tinstagramId: " + instagramId +
+                "\n\ttwitterId: " + twitterId +
                 super.toString();
     }
 }
