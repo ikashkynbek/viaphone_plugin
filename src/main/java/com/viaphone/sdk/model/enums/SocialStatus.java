b6b2c8680fbf1a3cb8fab21d80dab2210d7543b8
ik <ikashkynbek@gmail.com>
2016-09-30 14:03:59 +0600

    removed social statuses

--- src/main/java/com/viaphone/sdk/model/enums/SocialStatus.java
+++ src/main/java/com/viaphone/sdk/model/enums/SocialStatus.java
@@ -2,11 +2,5 @@ package com.viaphone.sdk.model.enums;
 
 public enum SocialStatus {
     SINGLE,
-    IN_RELATIONSHIP,
-    ENGAGED,
-    MARRIED,
-    CIVIL_UNION,
-    SEPARATED,
-    DIVORCED,
-    WIDOWED
+    MARRIED
 }
